c9c38a9904ce15547566197cc440126a12431950
dianaovechkina <ovechkinadiana@yandex.ru>
2022-09-27 12:09:29 +0300

    удалила строки

--- Program.cs
+++ Program.cs
@@ -1,6 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-
-Console.WriteLine("Hello, German!");
-
-Console.WriteLine("I want to check!");
